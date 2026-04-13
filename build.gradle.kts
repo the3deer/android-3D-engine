3dd66dac465383f16a60ec60f9974ee9ef96eeee
Andrés Oviedo <andresoviedo@gmail.com>
2026-04-13 20:08:57 +0200

    package fixed

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+            isShrinkResources = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
